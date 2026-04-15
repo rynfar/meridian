0de65725c80e80a8a683041e8d6c7a46ce73e211
DreamingCodes <me@dreaming.codes>
2026-04-15 12:36:14 -0700

    fix: include plugins into nix package

--- flake.nix
+++ flake.nix
@@ -75,6 +75,7 @@
               mkdir -p $out/lib/meridian
               cp -r dist $out/lib/meridian/
               cp -r node_modules $out/lib/meridian/
+              cp -r plugin $out/lib/meridian/
               cp package.json $out/lib/meridian/
 
               mkdir -p $out/bin
